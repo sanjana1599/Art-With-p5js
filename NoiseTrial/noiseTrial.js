077bc39d74f99c7b4d853df78bb9fb916c9c3fb9
sanjana1599 <42425709+sanjana1599@users.noreply.github.com>
2020-10-02 08:40:16 +0530

    Update noiseTrial.js

--- NoiseTrial/noiseTrial.js
+++ NoiseTrial/noiseTrial.js
@@ -22,27 +22,10 @@ function setup()
 	background(bgCol[colNum].r, bgCol[colNum].g, bgCol[colNum].b);
 	strokeWeight(2);
 
-	//Noisy Cloud Texture
-	// for (let i = gap; i < width - gap; i += 1)
-	// {
-	// 	for (let j = gap; j < height - gap; j += 1)
-	// 	{
-	// 		let colr = map(noise(i/100, j/100), 0, 1, 0, 255);
-	// 		stroke(100, colr, 200);
-	// 		fill(100, colr, 200);
-	// 		ellipse(i, j, 4, 4);			
-	// 	}
-	// }
-
-	//Stripes
 	for (let i = gap; i < width - gap; i += 3)
 	{
 		for (let j = gap; j < height - gap; j += 1)
 		{
-			// noStroke();
-			// fill(200, 45, 150);
-			// ellipse(i, j, 2, 2);
-
 			let i2 = i + map(noise(j/80, i/80), 0, 1, -50, 50);
 			let j2 = j + map(noise(i/80, j/80), 0, 1, -50, 50); 
 			noStroke();
@@ -59,10 +42,4 @@ function keyPressed()
 	{
 		saveFrames('out', 'png');
 	}
-}
-
-
-function draw()
-{
-
 }
